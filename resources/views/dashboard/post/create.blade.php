e8668f7372333a949dff0524143919d94592d2b6
justinornelas10 <jose.ornelas@cceo.com.mx>
2025-01-23 10:12:58 -0600

    feat: :sparkles: Add a form and validation

---
+++ resources/views/dashboard/post/create.blade.php
@@ -0,0 +1,50 @@
+@extends('dashboard.layout')
+
+@section('content')
+    @if ($errors->any())
+        @foreach ($errors->all() as $e)
+            <div>
+                {{ $e }}
+            </div>
+        @endforeach
+    @endif
+
+    <form action="{{ route('post.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label for="">Title</label>
+            <input type="text" name="title">
+        </div>
+        <div>
+            <label for="">Slug</label>
+            <input type="text" name="slug">
+        </div>
+        <div>
+            <label for="">Content</label>
+            <textarea name="content"></textarea>
+
+            <label for="">Description</label>
+            <textarea name="description"></textarea>
+        </div>
+
+        <div>
+            <label for="">Category</label>
+            <select name="category_id" id="">
+               @foreach ($categories as $title => $id)
+                   <option value="{{ $id }}">{{ $title }}</option>
+               @endforeach
+            </select>
+
+            <label for="">Posted</label>
+            <select name="posted">
+                <option value="not">No</option>
+                <option value="yes">Yes</option>
+            </select>
+        </div>
+
+        <button type="submit">Send</button>
+
+
+    </form>
+@endsection
