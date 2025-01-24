1663154fdeeb00d36c95ca2e7a26857f8fa60f18
justinornelas10 <jose.ornelas@cceo.com.mx>
2025-01-24 15:02:54 -0600

    feat: :sparkles: Save previous values for Post/edit

--- resources/views/dashboard/post/_form.blade.php
+++ resources/views/dashboard/post/_form.blade.php
@@ -2,32 +2,32 @@
 
 <div>
     <label for="">Title</label>
-    <input type="text" name="title" value="{{ $post-> title }}">
+    <input type="text" name="title" value="{{ old('title',$post->title) }}">
 </div>
 <div>
     <label for="">Slug</label>
-    <input type="text" name="slug" value="{{ $post-> slug }}">
+    <input type="text" name="slug" value="{{ old('slug', $post->slug) }}">
 </div>
 <div>
     <label for="">Content</label>
-    <textarea name="content" >{{ $post-> content }}</textarea>
+    <textarea name="content" >{{ old('content', $post->content) }}</textarea>
 
     <label for="">Description</label>
-    <textarea name="description">{{ $post-> description }}</textarea>
+    <textarea name="description">{{ old('description',$post-> description) }}</textarea>
 </div>
 
 <div>
     <label for="">Category</label>
     <select name="category_id" id="">
        @foreach ($categories as $title => $id)
-           <option {{ $post->category ==  $id ? 'selected' : null}} value={{ $id }}>{{ $title }}</option>
+           <option {{ old('category',$post->category_id) ==  $id ? 'selected' : null}} value={{ $id }}>{{ $title }}</option>
        @endforeach
     </select>
 
     <label for="">Posted</label>
     <select name="posted">
-        <option {{ $post->posted ==  'not' ? 'selected' : null}} value="not">No</option>
-        <option {{ $post->posted ==  'yes' ? 'selected' : null}} value="yes">Yes</option>
+        <option {{ old('posted',$post->posted) ==  'not' ? 'selected' : null}} value="not">No</option>
+        <option {{ old('posted',$post->posted) ==  'yes' ? 'selected' : null}} value="yes">Yes</option>
     </select>
 </div>
 
